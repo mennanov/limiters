1856878c83489f9aa294d605e87c263789f560da
VivaLaPanda <adrianrsmith93@gmail.com>
2023-03-15 16:46:28 -0700

    Various fixes to support newer redis versions

--- tokenbucket.go
+++ tokenbucket.go
@@ -367,7 +367,7 @@ func (t *TokenBucketRedis) State(ctx context.Context) (TokenBucketState, error) 
 		if t.raceCheck {
 			keys = append(keys, redisKey(t.prefix, redisKeyTBVersion))
 		}
-		values, err = t.cli.MGet(keys...).Result()
+		values, err = t.cli.MGet(ctx, keys...).Result()
 	}()
 
 	select {
@@ -419,17 +419,17 @@ func (t *TokenBucketRedis) SetState(ctx context.Context, state TokenBucketState)
 	go func() {
 		defer close(done)
 		if !t.raceCheck {
-			_, err = t.cli.TxPipelined(func(pipeliner redis.Pipeliner) error {
-				if err = pipeliner.Set(redisKey(t.prefix, redisKeyTBLast), state.Last, t.ttl).Err(); err != nil {
+			_, err = t.cli.TxPipelined(ctx, func(pipeliner redis.Pipeliner) error {
+				if err = pipeliner.Set(ctx, redisKey(t.prefix, redisKeyTBLast), state.Last, t.ttl).Err(); err != nil {
 					return err
 				}
-				return pipeliner.Set(redisKey(t.prefix, redisKeyTBAvailable), state.Available, t.ttl).Err()
+				return pipeliner.Set(ctx, redisKey(t.prefix, redisKeyTBAvailable), state.Available, t.ttl).Err()
 			})
 			return
 		}
 		var result interface{}
 		// TODO: use EVALSHA.
-		result, err = t.cli.Eval(`
+		result, err = t.cli.Eval(ctx, `
 	local version = tonumber(redis.call('get', KEYS[1])) or 0
 	if version > tonumber(ARGV[1]) then
 		return 'RACE_CONDITION'
