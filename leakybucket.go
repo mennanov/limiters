31a821c525dee755d4609d0564354cb576419703
Joshua Hyde <jrh3k5@gmail.com>
2025-07-09 12:55:49 -0500

    Use UNIX epoch seconds for expiration

--- leakybucket.go
+++ leakybucket.go
@@ -589,7 +589,7 @@ func (t *LeakyBucketMemcached) SetState(ctx context.Context, state LeakyBucketSt
 			Key:        t.key,
 			Value:      b.Bytes(),
 			CasID:      t.casId,
-			Expiration: int32(t.ttl.Seconds()),
+			Expiration: int32(time.Now().Add(t.ttl).Unix()),
 		}
 		if t.raceCheck && t.casId > 0 {
 			err = t.cli.CompareAndSwap(item)
